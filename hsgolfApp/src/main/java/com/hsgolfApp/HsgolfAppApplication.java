47d4a436ee87197dca3cea114b3483dd05aa0557
mike <mike@DESKTOP-H98JHIB>
2019-03-06 17:11:11 -0800

    upload hsgolf app

---
+++ hsgolfApp/src/main/java/com/hsgolfApp/HsgolfAppApplication.java
@@ -0,0 +1,13 @@
+package com.hsgolfApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HsgolfAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HsgolfAppApplication.class, args);
+	}
+
+}
